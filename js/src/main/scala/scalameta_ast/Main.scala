b2e54be9b6b06252dc9f1f0bf60288f2fddfdca1
xuwei-k <6b656e6a69@gmail.com>
2022-10-22 07:42:48 +0900

    fix warnings

--- js/src/main/scala/scalameta_ast/Main.scala
+++ js/src/main/scala/scalameta_ast/Main.scala
@@ -1,11 +1,13 @@
 package scalameta_ast
 
+import scala.annotation.nowarn
 import scala.scalajs.js
 import scala.scalajs.js.annotation._
 
 @JSExportTopLevel("ScalametaAstMain")
 object Main {
   @JSExport
+  @nowarn
   def convert(source: String): js.Object = {
     val output = new ScalametaAST().convert(source)
     new js.Object {
