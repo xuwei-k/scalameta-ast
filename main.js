49c18f2750e14ab3375522bd061ccc18ceeb0b84
xuwei-k <6b656e6a69@gmail.com>
2022-11-12 23:29:59 +0000

    Deploying to gh-pages from @ xuwei-k/scalameta-ast@eb22c71b9c18f51e0d14ce9fe7b608906b20ba70 🚀

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ $(function(){
 
   $("#format_input").click(function(){
     const input = $("#input_scala").val();
-    const result = ScalametaAstMain.format(input);
+    const scalafmt = $("#scalafmt").val();
+    const result = ScalametaAstMain.format(input, scalafmt);
     if (input != result) {
       $("#input_scala").val(result);
     }
@@ -14,11 +15,13 @@ $(function(){
 
   const run = function(){
     try {
-      const input = $("#input_scala").val()
+      const scalafmt = $("#scalafmt").val();
+      const input = $("#input_scala").val();
 
       const r = ScalametaAstMain.convert(
         input,
-        $("#format").prop("checked") === true
+        $("#format").prop("checked") === true,
+        scalafmt
       );
       $("#output_scala").text(r.ast);
       $("#info").text(`ast: ${r.astBuildMs} ms\nfmt: ${r.formatMs} ms`)
@@ -30,6 +33,13 @@ $(function(){
       if (input.length < saveLimit) {
         try {
           localStorage.setItem("source", input);
+        } catch(e) {
+          console.trace(e);
+        }
+      }
+      if (scalafmt.length < saveLimit) {
+        try {
+          localStorage.setItem("scalafmt", scalafmt);
         } catch(e) {
           console.trace(e);
         }
@@ -54,6 +64,14 @@ $(function(){
 
   $(document).ready(function(){
     const savedSource = localStorage.getItem("source");
+    const savedScalafmt = localStorage.getItem("scalafmt");
+
+    if (savedScalafmt != null) {
+      $("#scalafmt").val(savedScalafmt);
+    } else {
+      $("#scalafmt").val('{\n  "maxColumn" : 50\n}');
+    }
+
     if (savedSource != null) {
       $("#input_scala").val(savedSource);
     } else {
