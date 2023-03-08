22abd23aebeb7882ab464b998a0076010314c986
xuwei-k <6b656e6a69@gmail.com>
2023-03-08 13:34:38 +0900

    avoid wildcard import

---
+++ shared/src/test/scala/scalameta_ast/ScalametaASTSpec.scala
@@ -0,0 +1,39 @@
+package scalameta_ast
+
+import org.scalatest.freespec.AnyFreeSpec
+import scala.meta.Defn
+import scala.meta.Tree
+import scala.meta.Pkg
+import scala.meta.dialects
+import scala.meta.inputs.Input
+
+class ScalametaASTSpec extends AnyFreeSpec {
+  "ScalametaAST" - {
+    "top level scalameta classes" in {
+      val url =
+        "https://raw.githubusercontent.com/scalameta/scalameta/v4.7.5/scalameta/trees/shared/src/main/scala/scala/meta/Trees.scala"
+      val src = scala.io.Source.fromURL(url, "UTF-8").getLines().mkString("\n")
+      val parsed =
+        implicitly[scala.meta.parsers.Parse[scala.meta.Source]].apply(Input.String(src), dialects.Scala213Source3).get
+
+      def topLevel(t: Tree): Boolean =
+        t.parent.exists {
+          case p: Pkg =>
+            p.name.value == "meta"
+          case _ =>
+            false
+        }
+
+      val values = parsed.collect {
+        case c: Defn.Class if topLevel(c) =>
+          c.name.value
+        case c: Defn.Trait if topLevel(c) =>
+          c.name.value
+        case c: Defn.Object if topLevel(c) =>
+          c.name.value
+      }.toSet
+      val expect = (new ScalametaAST).topLevelScalametaDefinitions.map(_.getSimpleName).toSet
+      assert(values == expect)
+    }
+  }
+}
