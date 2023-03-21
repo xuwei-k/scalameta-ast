11a2a426131890ff32ddfa07a8df957931c68200
kenji yoshida <6b656e6a69@gmail.com>
2023-03-21 11:22:42 +0900

    add more test

--- core/src/test/scala/scalameta_ast/ScalametaASTSpec.scala
+++ core/src/test/scala/scalameta_ast/ScalametaASTSpec.scala
@@ -9,6 +9,54 @@ import scala.meta.inputs.Input
 
 class ScalametaASTSpec extends AnyFreeSpec {
   "ScalametaAST" - {
+    val main = new ScalametaAST
+    "convert" in {
+      val result = main.convert(
+        src = "val x = y",
+        format = true,
+        scalafmtConfig = metaconfig.Conf.Obj.empty,
+        outputType = "syntactic",
+        packageName = Option("package_name"),
+        wildcardImport = false,
+        ruleNameOption = None,
+        dialect = None,
+        patch = None,
+      )
+      val expect = s"""package package_name
+         |
+         |import scala.meta.Defn
+         |import scala.meta.Pat
+         |import scala.meta.Term
+         |import scalafix.Patch
+         |import scalafix.lint.Diagnostic
+         |import scalafix.lint.LintSeverity
+         |import scalafix.v1.SyntacticDocument
+         |import scalafix.v1.SyntacticRule
+         |
+         |class Example extends SyntacticRule("Example") {
+         |  override def fix(implicit doc: SyntacticDocument): Patch = {
+         |    doc.tree.collect {
+         |      case t @ Defn.Val(
+         |            Nil,
+         |            List(Pat.Var(Term.Name("x"))),
+         |            None,
+         |            Term.Name("y")
+         |          ) =>
+         |        Patch.lint(
+         |          Diagnostic(
+         |            id = "",
+         |            message = "",
+         |            position = t.pos,
+         |            explanation = "",
+         |            severity = LintSeverity.Warning
+         |          )
+         |        )
+         |    }.asPatch
+         |  }
+         |}
+         |""".stripMargin
+      assert(result.ast == expect)
+    }
     (0 to 1).foreach { i =>
       s"top level scalameta classes ${i}" in {
         val src = TestCompat.scalametaTreeFile(i)
@@ -31,7 +79,7 @@ class ScalametaASTSpec extends AnyFreeSpec {
           case c: Defn.Object if topLevel(c) =>
             c.name.value
         }.toSet
-        val expect = (new ScalametaAST).topLevelScalametaDefinitions.map(_.getSimpleName).toSet
+        val expect = main.topLevelScalametaDefinitions.map(_.getSimpleName).toSet
         assert(values == expect)
       }
     }
