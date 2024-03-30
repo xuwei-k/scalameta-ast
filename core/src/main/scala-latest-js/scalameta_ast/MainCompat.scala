d39c32ebcc0e6b34ce3508db93557381e4a71b5a
xuwei-k <6b656e6a69@gmail.com>
2024-03-30 20:18:37 +0900

    wip [skip ci]

--- core/src/main/scala-latest-js/scalameta_ast/MainCompat.scala
+++ core/src/main/scala-latest-js/scalameta_ast/MainCompat.scala
@@ -6,12 +6,71 @@ import org.ekrich.config.ConfigFactory
 import org.ekrich.config.ConfigRenderOptions
 import org.scalafmt.config.ScalafmtConfig
 import scala.annotation.nowarn
+import scala.meta.common.Convert
+import scala.meta.contrib.XtensionTreeOps
+import scala.meta.parsers.Parse
 import scala.scalajs.js
 import scala.scalajs.js.JSON
 import scala.scalajs.js.annotation._
 import scala.util.control.NonFatal
 
 trait MainCompat {
+
+  @JSExport
+  def rawWithPos(
+    src: String,
+    dialect: String,
+    scalafmtConfig: String,
+    line: Int,
+    column: Int,
+  ) = {
+    import scala.meta._
+    val convert = implicitly[Convert[String, Input]]
+    val main = new ScalametaAST
+    val dialects = {
+      main.stringToDialects.getOrElse(
+        dialect, {
+          Console.err.println(s"invalid dialct ${dialect}")
+          main.dialectsDefault
+        }
+      )
+    }
+
+    val input = convert.apply(src)
+    val tree: Tree = main.loopParse(
+      input,
+      for {
+        x1 <- main.parsers
+        x2 <- dialects
+      } yield (x1, x2)
+    )
+    val res: String = runFormat(
+      source = tree.structure,
+      scalafmtConfig = hoconToMetaConfig(scalafmtConfig)
+    ).result
+    val parsed: Term = implicitly[Parse[Term]].apply(Input.String(res), scala.meta.dialects.Scala3).get
+    val tokens = parsed.tokens
+    val cursorPos = {
+      if (src.isEmpty) {
+        Position.Range(input, 0, 0)
+      } else if (line >= src.linesIterator.size) {
+        Position.Range(input, src.length, src.length)
+      } else {
+        Position.Range(input, line - 1, column, line, column)
+      }
+    }
+    println(Seq("line" -> line, "column" -> column, "pos" -> cursorPos))
+    tree.collect {
+      case x if cursorPos.start <= x.pos.start =>
+        x
+    }.groupBy(_.pos.start).minByOption(_._1).map(_._2.minBy(_.pos.end).structure).map { current =>
+      println("current = " + current)
+      val currentSize = current.length
+      val count = tree.structure.sliding(currentSize).count(_ == current)
+      println(Seq("count" -> count, "size" -> currentSize))
+    }
+  }
+
   def runFormat(source: String, scalafmtConfig: Conf): Output[String] = {
     ScalametaAST.stopwatch {
       runFormat(
