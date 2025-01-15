545996aa87445a583965c3b7ec89f0a07d4636b8
xuwei-k <6b656e6a69@gmail.com>
2025-01-15 02:18:18 +0000

    Deploying to gh-pages from @ xuwei-k/scalameta-ast@67c64095aeb306e1203de31beae1921029633af1 🚀

--- latest/build_info.js
+++ latest/build_info.js
@@ -1,4 +1,4 @@
 export default {
-  "gitHash" : "51aa42f05901b22eabf388de705947da19438d6c",
-  "scalametaVersion" : "4.10.2"
+  "gitHash" : "67c64095aeb306e1203de31beae1921029633af1",
+  "scalametaVersion" : "4.12.4.1"
 }
