85355dc5178998a7444620a2ba01b667d6186bb6
xuwei-k <6b656e6a69@gmail.com>
2026-01-28 18:02:47 +0900

    sbt 2

--- core/src/test/js/scalameta_ast/TestCompat.scala
+++ core/src/test/js/scalameta_ast/TestCompat.scala
@@ -5,6 +5,8 @@ import scalajs.js.Dynamic
 object TestCompat {
   private val fs = Dynamic.global.require("fs")
   def scalametaTreeFile(i: Int): String = {
-    fs.readFileSync(s"core/target/jvm-2.13/resource_managed/test/trees${i}.scala").toString
+    fs.readFileSync(
+      s"target/out/jvm/scala-2.13.18/scalameta-ast/resource_managed/test/trees${i}.scala"
+    ).toString
   }
 }
